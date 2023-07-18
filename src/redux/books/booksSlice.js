9c19f1421873bdc10fbc8790c8f7767069447568
yemidada <yemidada@gmail.com>
2023-07-18 14:49:47 +0100

    connect redux and component together

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -4,6 +4,24 @@ export const booksSlice = createSlice({
   name: 'books',
   initialState: {
     books: [
+      {
+        item_id: 'item1',
+        title: 'The Great Gatsby',
+        author: 'John Smith',
+        category: 'Fiction',
+      },
+      {
+        item_id: 'item2',
+        title: 'Anna Karenina',
+        author: 'Leo Tolstoy',
+        category: 'Fiction',
+      },
+      {
+        item_id: 'item3',
+        title: 'The Selfish Gene',
+        author: 'Richard Dawkins',
+        category: 'Nonfiction',
+      },
     ],
   },
   reducers: {
@@ -11,7 +29,7 @@ export const booksSlice = createSlice({
       state.books.push(action.payload);
     },
     remove: (state, action) => {
-      state.books = state.books.filter((book) => book.id !== action.payload);
+      state.books = state.books.filter((book) => book.item_id !== action.payload.item_id);
     },
   },
 });
